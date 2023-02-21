40b245bb18dd2d03fbb41fb59be6bae2f25ee482
TopperClausen <topperhdoriginal@gmail.com>
2023-02-21 18:52:12 +0100

    lol

--- bot/commands/ping.js
+++
@@ -1,5 +0,0 @@
-const execute = (message, args) => {
-  message.channel.send('Pong!');
-}
-
-module.exports = { execute }
--- bot/events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  execute: (event, client) => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("TurlteBot", { type: "WATCHING" });
-  }
-}
