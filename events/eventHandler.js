e2e5032fdc5183178318d0d9e9998ed31ef5b2ec
TopperClausen <topperhdoriginal@gmail.com>
2023-04-13 17:18:14 +0200

    whoops

--- events/eventHandler.js
+++
@@ -1,6 +0,0 @@
-const loadEvents = (client) => {
-  client.on('ready', event => require('./ready').execute(event, client));
-  client.on('guildMemberAdd', guildMemberAdd => require('./guildMemberAdd').execute(guildMemberAdd, client));
-}
-
-module.exports = { loadEvents }
--- events/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  execute: (guildMemberAdd, client) => {
-    console.log(guildMemberAdd)
-    const role = guildMemberAdd.guild.roles.cache.find(role => role.name === 'Teenage Turles');
-    guildMemberAdd.addRole(role);
-  }
-}
